c125ca3a5851d68f363074d7fddee91899349ba5
shiyou0130011 <shiyou0130011+github@googlemail.com>
2016-06-24 10:57:01 +0800

    修改成 typescript 的 class 格式

--- NCR.ts
+++ NCR.ts
@@ -1,41 +1,39 @@
+let leadAdd = 0xD800, trailAdd = 0xDC00, ncrSubtract = 0x10000
+
+/**
+ * 將U+10000到U+10FFFF的碼位轉為字元值參照 (Numeric Character Reference, NCR)
+ * 
+ * @param hexadecimal 是否輸出成 16 進制的 NCR
+ */
+function encodeUTF16(leadSurrogates: number, trailSurrogates: number, hexadecimal: boolean  = false) {
+	if (leadSurrogates < leadAdd || trailSurrogates < trailAdd) {
+		return String.fromCharCode(leadSurrogates) + String.fromCharCode(trailSurrogates)
+	}
+	
+	leadSurrogates -= leadAdd
+	trailSurrogates -= trailAdd
+	let ncrNum = (leadSurrogates * 1024 + trailSurrogates) + ncrSubtract
+	if(hexadecimal){
+		return "&#x" + ncrNum.toString(16) + ";"
+	}
+	return "&#" + ncrNum + ";"
+}
+
+/**
+ * 將NCR 轉為字元
+ * 
+ */
+function decodeUTF16(num: number): string {
+	let lead, trail;
+	num -= ncrSubtract;
+	lead = Math.floor(num / 1024) + leadAdd;
+	trail = num % 1024 + trailAdd;
+	return String.fromCharCode(lead) + String.fromCharCode(trail)
+}
 /**
  * 字元值參照 (Numeric Character Reference, NCR) 轉換
  */
-var NCR = new function NCR() {
-	var leadAdd = 0xD800, trailAdd = 0xDC00, ncrSubtract = 0x10000
-
-	
-	/**
-	 * 將U+10000到U+10FFFF的碼位轉為字元值參照 (Numeric Character Reference, NCR)
-	 * 
-	 * @param hexadecimal 是否輸出成 16 進制的 NCR
-	 */
-	function encodeUTF16(leadSurrogates: number, trailSurrogates: number, hexadecimal: boolean  = false) {
-		if (leadSurrogates < leadAdd || trailSurrogates < trailAdd) {
-			return String.fromCharCode(leadSurrogates) + String.fromCharCode(trailSurrogates)
-		}
-		
-		leadSurrogates -= leadAdd
-		trailSurrogates -= trailAdd
-		var ncrNum = (leadSurrogates * 1024 + trailSurrogates) + ncrSubtract
-		if(hexadecimal){
-			return "&#x" + ncrNum.toString(16) + ";"
-		}
-		return "&#" + ncrNum + ";"
-	}
-	
-	/**
-	 * 將NCR 轉為字元
-	 * 
-	 */
-	function decodeUTF16(num: number): string {
-		var lead, trail;
-		num -= ncrSubtract;
-		lead = Math.floor(num / 1024) + leadAdd;
-		trail = num % 1024 + trailAdd;
-		return String.fromCharCode(lead) + String.fromCharCode(trail)
-	}
-	
+class NCR{
 	/**
 	 * 將字串 str 特殊字元編譯成字元值參照
 	 * @param str 待轉換的 String
@@ -50,8 +48,8 @@ var NCR = new function NCR() {
 	 * // return "&amp;#x10000;"
 	 * NCR.encode("\uD83C\uDC00", true)
 	 */
-	this.encode = function (str: string, hexadecimal: boolean = false): string{
-		for(var i = 0; i < str.length; i++){
+	encode(str: string, hexadecimal: boolean = false): string{
+		for(let i = 0; i < str.length; i++){
 			if(str.charCodeAt(i) >= leadAdd && str.charCodeAt(i + 1) >= trailAdd){
 				str = str.replace(
 					str.substr(i, 2),
@@ -77,10 +75,10 @@ var NCR = new function NCR() {
 	 * // return "你好"
 	 * NCR.decode("&amp;#20320;&amp;#22909;")
 	 */
-	this.decode = function (str: string): string{
+	decode(str: string): string{
 		return str
 			.replace(/&#x(\w*);/g, function (match, s) {
-				var num = parseInt(s, 16)
+				let num = parseInt(s, 16)
 				if (num <= 0xffff) {
 					return String.fromCharCode(num)
 				}
